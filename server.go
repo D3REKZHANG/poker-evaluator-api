694866703305b7977a9522fcdfb02ec29f9de27a
Derek Zhang <derek.zhang0210@gmail.com>
2021-10-01 23:58:08 -0400

    add basic routing

--- server.go
+++ server.go
@@ -6,12 +6,24 @@ import (
     "github.com/labstack/echo/v4"
 )
 
+type Hand struct {
+    Cards string `json:"cards"`
+}
+
+func rankHand(c echo.Context) error {
+    u := &Hand{
+        Cards: "2h",
+    }
+    return c.JSON(http.StatusOK, u)
+}
+
 func main() {
 
-    e : echo.New()
+    e := echo.New()
     e.GET("/", func(c echo.Context) error{
-        return c.String(http.StatusOK, "Hello World!")
+        return c.String(http.StatusOK, "Poker Evaluator API")
     })
+    e.GET("/rankHand", rankHand)
     e.Logger.Fatal(e.Start(":1323"))
 
 }
