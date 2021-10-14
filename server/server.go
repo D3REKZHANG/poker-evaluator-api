34c1f3745bea4ea1ec82055641ca761e7d081bbf
Derek Zhang <derek.zhang0210@gmail.com>
2021-10-13 20:24:02 -0400

    restructure to 2.5 tier architecture

---
+++ server/server.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+    "net/http"
+    "github.com/labstack/echo/v4"
+    "local/views"
+    "local/business"
+)
+
+func main() {
+
+    business.LoadStore()
+
+    e := echo.New()
+    
+    e.GET("/", func(c echo.Context) error{
+        return c.String(http.StatusOK, "Poker Evaluator API")
+    })
+    e.POST("/rankHand", views.RankHand)
+
+    e.Logger.Fatal(e.Start(":1323"))
+
+}
