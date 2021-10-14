460ab684a1f83ac9df17c8f1b4d8e110dd5fdc67
Derek Zhang <derek.zhang0210@gmail.com>
2021-10-13 20:23:39 -0400

    restructure to 2.5 tier architecture

--- test.py
+++ test.py
@@ -1,3 +1,3 @@
 import requests
 
-print(requests.post('http://localhost:1323/rankHand', json={'cards': ['2h', '4s']}).text)
+print(requests.post('http://localhost:1323/rankHand', json={'cards': ['2h', '4s', '5d', 'Kh', 'As']}).text)
